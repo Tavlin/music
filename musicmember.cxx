ba26577715abea4f1ef3e606b729c1e7602dd0a2
Tavlin <hphemmer@aol.com>
2018-07-07 00:23:30 +0200

    weird bug with ; before :: at list[i] no idea why

--- musicmember.cxx
+++ musicmember.cxx
@@ -44,24 +44,44 @@ class Musicmember
     }
 
     // function to add a Genre to the list of genres
-    void AddGenre(string added_genre){
+    void AddGenre(string added_genre)
+    {
       Musicmember::genres.push_back(added_genre);
     }
 
     // function to add additional info about the band
-    void AddBandInfo(string added_bandinfo){
+    void AddBandInfo(string added_bandinfo)
+    {
       Musicmember::bandinfo.append("\n");
       Musicmember::bandinfo.append(added_bandinfo);
     }
 
     // function to change the name of the band if you missspelled it
-    void ChangeBandname(string new_bandname){
+    void ChangeBandname(string new_bandname)
+    {
       Musicmember::bandname = new_bandname;
     }
 
     // function to change the info of the band
-    void ChangeBandinfo(string new_bandinfo){
+    void ChangeBandinfo(string new_bandinfo)
+    {
       Musicmember::bandinfo = new_bandinfo;
+    }
+
+    void PrintMusicmember()
+    {
+      int size_genres = Musicmember::genres.size();
+      for (int i = 0; i < size_genres; i++) {
+        if(i < size_genres - 1){
+          cout << Musicmember::genres[i] << " ";
+        }
+        else{
+          cout << Musicmember::genres[i] << ":" << endl;
+        }
+
+      }
+      cout << Musicmember::bandname << endl;
+
     }
 
 
@@ -75,7 +95,24 @@ Musicmember::Musicmember(string a, vector<string> b, string c, vector<song> d){
   songlist = d;
 }
 
-vector<Musicmember> list;
+vector<Musicmember> musiclist;
+
+void search_genre(vector<Musicmember> list, string searched_genre)
+{
+  int size_list = list.size();
+  int size_genres = 0;
+  for (int i = 0; i < size_list; i++)
+  {
+    list[i]::genres.size() = size_genres;
+    for (int j = 0; j < size_genres; j++)
+    {
+      if(list[i]::genres[j].compare(searched_genre) == 0)
+      {
+        list[i]::PrintMusicmember();
+      }
+    }
+  }
+}
 
 
 int main(int argc, char* argv[]){
